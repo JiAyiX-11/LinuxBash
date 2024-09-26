643c31d86aa641ff3e35a23aec9d9e07d7a96ec3
JiAyi <160787619+JiAyiX-2001@users.noreply.github.com>
2024-09-27 00:32:18 +0200

    Add files via upload

---
+++ inst.sh
@@ -0,0 +1,17 @@
+sudo apt-get install gnome-tweaks 
+
+sudo apt-get install tree
+
+sudo apt-get install net-tools
+
+sudo apt-get install git
+
+sudo apt install exa
+
+sudo apt install wget
+
+sudo cp -rfv skin/tema_JiAyi /usr/share/themes && sudo cp -rfv skin/cursor_JiAyi /usr/share/icons
+
+cat bashrc.txt > ~/.bashrc && source ~/.bashrc
+
+gnome-tweaks
